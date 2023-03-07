6da04d2f3370841540ff01586f5baf98663cb9f1
Letam <davidsonletam@gmail.com>
2023-03-07 23:21:07 +0100

    margin layout on small screens

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import NavBar from "../components/NavBar";
 
 const Layout = ({ children }) => {
 	return (
-		<div className="mx-0 sm:mx-20 mt-5">
+		<div className="mx-5 sm:mx-20 mt-5">
 			<NavBar />
 			<main>{children}</main>
 		</div>
