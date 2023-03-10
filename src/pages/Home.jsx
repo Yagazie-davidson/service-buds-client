f342ba8bb47ce2b469b196a92f0bcee7ca305bb1
Letam <davidsonletam@gmail.com>
2023-03-10 13:31:23 +0100

    hero content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,13 @@ function Home() {
 		<Layout>
 			<main className="flex flex-col justify-center items-center mt-32">
 				<h1 className="font-extrabold text-6xl tablet:text-8xl text-center text-black max-w-6xl">
-					A town hall different from bala blu
+					All the information you need in one Place
 				</h1>
 				<p className="text-center text-asht font-normal text-lg mt-5 mx-2 tablet:mx-28">
-					The farmers will make more money. Your lunch will not be imported,
-					cassava garri ewa and ehhh ehhhhnn. The farmer will make money, the
-					dinner would be cassava, eba, ewa and everything.
+					Having issues? Need help? Want assistance? Want a service provider?
+					Get a provider in you Hostel through service buds! Check out our List
+					today to see services available to your hostel and contact
+					information.
 				</p>
 				<Link to="/services">
 					<Button text={"Explore Services"} className={"mt-10"} />
